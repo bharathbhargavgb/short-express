cb73d50e9facb7e3ca86136fcaaa880270fad929
Bharath Bhargav <baaskab@amazon.com>
2021-07-03 22:49:04 +0530

    Pipe request to API gateway

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'URL Shortener' });
+  res.render('index');
+});
+
+router.post('/', function(req, res) {
+  console.log("request received");
+  axios({
+    method: 'post',
+    data: req.body,
+    url: 'https://angh4tqiu8.execute-api.us-east-1.amazonaws.com/staging/shorten',
+    responseType: "stream",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((axiosResponse) => {
+    axiosResponse.data.pipe(res);
+  });
 });
 
 module.exports = router;
