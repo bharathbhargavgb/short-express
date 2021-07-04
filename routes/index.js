577beac98f8f10024d44f460600b1ed40efd7c77
Bharath Bhargav <baaskab@amazon.com>
2021-07-04 17:00:28 +0530

    serve index.html

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index.html');
 });
 
 router.post('/', function(req, res) {
